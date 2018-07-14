2c51f9f0e4fa1d8281e4e68656a78dd484388fd4
EvanHungate <hungate.evan@gmail.com>
2018-07-13 22:13:40 -0400

    bakery

--- lessons/week-1/react/playground/src/App.js
+++ lessons/week-1/react/playground/src/App.js
@@ -3,35 +3,11 @@ import { render } from 'react-dom'; //need to specify what you're using react fo
 import logo from './logo.svg';
 import './App.css';
 
-class Counter extends React.Component {
-	constructor(){
-		super();
-		this.state={
-			count: 5
-		}
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick(){
-		this.setState({count: this.state.count + 1})
-	}
+class FeaturedDonut extends React.Component{
 	render(){
 		return(
-				<div>
-					<p>Visitor Count : {this.state.count}</p>
-					<button onClick={this.handleClick}>Increment</button>
-				</div>
+				<div>feature is : PB & J</div>
 			)
-	}
-}
-
-class SearchBar extends React.Component {
-	render() {
-		return(
-			<div>
-				<input type="text" placeholder="enter term" name="" />
-			</div>
-		)
-
 	}
 }
 
@@ -39,9 +15,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Wazzaapppp</h1>
-                <SearchBar />
-                <Counter />
+                <FeaturedDonut />
             </div>
         )
     }
